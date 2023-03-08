951ab798141cec48d703a2ffdf409aee2dadc539
Chukwuma Akunyili <chukwuma.akunyili.233279@unn.edu.ng>
2023-03-08 10:48:44 +0100

    add: initial commit

---
+++ Nous-University/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Nous_University.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
